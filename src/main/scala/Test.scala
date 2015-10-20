aaef81f9a40b6e98aec7ad12f0c95ee9b43ca7ed
Brian Hsu <brianhsu.hsu@gmail.com>
2015-10-20 16:41:24 +0800

    Extract RS232Connection class.

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,20 +1,95 @@
 import jssc.SerialPort
+import jssc.SerialPort
+import jssc.SerialPortEvent
+import jssc.SerialPortEventListener
+
+
+class RS232Connection(port: String, baudRate: Int) {
+
+  def processResponse(callback: String => Any) {
+    val eventMask = 
+      SerialPortEvent.BREAK + SerialPortEvent.CTS + SerialPortEvent.DSR + SerialPortEvent.ERR +
+      SerialPortEvent.RING + SerialPortEvent.RLSD + SerialPortEvent.RXCHAR + SerialPortEvent.RXFLAG + 
+      SerialPortEvent.TXEMPTY 
+
+    val eventListener = new SerialPortEventListener {
+
+      val buffer = new StringBuffer
+
+      def processInputBuffer(byteCounts: Int) {
+        val data = serialPort.readString().filter(_ != '\r')
+
+        data.foreach { character =>
+          if (character == '\n') {
+            callback(buffer.toString)
+            buffer.setLength(0)
+          } else {
+            buffer.append(character)
+          }
+        }
+      }
+
+      override def serialEvent(event: SerialPortEvent) {
+        event.getEventType match {
+          case SerialPortEvent.BREAK => 
+            println("SerialPortEvent.BREAK" + event.getEventValue)
+          case SerialPortEvent.CTS   => 
+            println("SerialPortEvent.CTS:" + event.getEventValue)
+          case SerialPortEvent.DSR   => 
+            println("SerialPortEvent.DSR:" + event.getEventValue)
+          case SerialPortEvent.ERR   => 
+            println("SerialPortEvent.ERR:" + event.getEventValue)
+          case SerialPortEvent.RING  => 
+            println("SerialPortEvent.RING:" + event.getEventValue)
+          case SerialPortEvent.RLSD    => 
+            println("SerialPortEvent.RLSD:" + event.getEventValue)
+          case SerialPortEvent.RXFLAG  => 
+            println("SerialPortEvent.RXFLAG:" + event.getEventValue)
+          case SerialPortEvent.TXEMPTY => 
+            println("SerialPortEvent.TXEMPTY:" + event.getEventValue)
+          case SerialPortEvent.RXCHAR  => 
+            processInputBuffer(event.getEventValue)
+        }
+      }
+    }
+
+    serialPort.setEventsMask(eventMask)
+    serialPort.addEventListener(eventListener)
+  }
+
+  val serialPort = new SerialPort("/dev/ttyUSB0")
+
+  serialPort.openPort()
+  serialPort.setParams(baudRate, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE)
+
+  def sendCommand(command: String) {
+    serialPort.writeBytes(s"$command\n\r".getBytes)
+  }
+
+  def close() {
+    serialPort.closePort()
+  }
+
+}
+
 
 object Main {
-  def main(args: Array[String]) {
-    println("Hello World")
-    val serialPort = new SerialPort("/dev/ttyUSB0")
-    serialPort.openPort()
-    serialPort.setParams(
-      SerialPort.BAUDRATE_9600, 
-      SerialPort.DATABITS_8,
-      SerialPort.STOPBITS_1,
-      SerialPort.PARITY_NONE
-    )
 
-    serialPort.writeBytes("COMU?\n\r".getBytes)
-    serialPort.writeBytes("COMU:OVER\n\r".getBytes)
-    serialPort.writeBytes("COMU:OFF\n\r".getBytes)
-    serialPort.closePort()
+
+  def main(args: Array[String]) {
+
+    println("Hello World")
+
+    val rsConnection = new RS232Connection("/dev/ttyUSB0", SerialPort.BAUDRATE_38400)
+
+    rsConnection.processResponse { line =>
+      println("Got data...:" + line)
+    }
+    var count = 0
+    while (true) {
+      Thread.sleep(1000)
+      count += 1
+    }
+
   }
 }
